42f85630e27ea614c6eb8bf4de0d5f568968a639
花落永恒 <zjh@hualaoyongheng.local>
2017-10-24 11:28:54 +0800

    first commit

--- MyShow/AppDelegate.m
+++ MyShow/AppDelegate.m
@@ -16,7 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"划落永恒");
     return YES;
 }
 
